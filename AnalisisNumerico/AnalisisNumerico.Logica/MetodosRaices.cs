3a007de1f251d6ea5ce6fbb771c2b667706f27fd
SantiagoCastellano <santiagocastellano01@gmail.com>
2018-08-17 12:55:19 -0300

    Termina metodo Buscar Raices

--- AnalisisNumerico/AnalisisNumerico.Logica/MetodosRaices.cs
+++ AnalisisNumerico/AnalisisNumerico.Logica/MetodosRaices.cs
@@ -23,10 +23,45 @@ namespace AnalisisNumerico.Logica
         }
         private Resultado BuscarRaices(ParametrosBiseccion parametros)
         {
+            Resultado resultado = new Resultado();
+            resultado.ErrorRelativo = 0;
+            resultado.Iteraciones = 0;
             int contador = 0;
             double anterior = 0;
+            var XI = parametros.Xi;
+            var XD = parametros.Xd;
             var xr = (parametros.Xd + parametros.Xi) / 2;
             var errorRelativo = (xr - anterior) / xr;
+            var resultadoXR = this.EvaluarFuncion(parametros.Funcion, xr);
+            if (resultadoXR < parametros.Tolerancia)
+            {
+                resultado.Raiz = xr;
+                return resultado;
+            }
+            while (errorRelativo < parametros.Tolerancia || contador > parametros.Iteraciones || resultadoXR < parametros.Tolerancia)
+            {
+                if (EvaluarFuncion(parametros.Funcion, XI)* EvaluarFuncion(parametros.Funcion,xr) > 0)
+                {
+                    XI = xr;
+                }
+                else
+                {
+                    XD = xr;
+                }
+                anterior = xr;
+                xr = (XI + XD) / 2;
+                contador += 1;
+                errorRelativo = (xr - anterior) / xr;
+                resultadoXR = this.EvaluarFuncion(parametros.Funcion, xr);
+                if (resultadoXR < 0)
+                {
+                    resultadoXR = resultadoXR * (-1);
+                }
+            }
+            resultado.Iteraciones = contador;
+            resultado.ErrorRelativo = errorRelativo;
+            resultado.Raiz = xr;
+            return resultado;
         }
            public Resultado MetodoBiseccion(ParametrosBiseccion parametros)
            {
@@ -56,10 +91,7 @@ namespace AnalisisNumerico.Logica
                    return resultado;
                }
 
-            
-
-
-
+            return this.BuscarRaices(parametros);
            }
            
     }
